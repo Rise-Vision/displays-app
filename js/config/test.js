2f9e73124d4b5547c986e9c7c35b9c3c9a4c22a3
adriandd <adriandd@gmail.com>
2015-10-28 15:14:51 -0400

    Update to Common Style 1.3.21 [stage-1]

--- js/config/test.js
+++ js/config/test.js
@@ -13,7 +13,8 @@
   'use strict';
 
   angular.module('risevision.common.i18n.config', [])
-    .constant('LOCALES_PREFIX', 'bower_components/rv-common-i18n/dist/locales/translation_')
+    .constant('LOCALES_PREFIX',
+      'bower_components/rv-common-i18n/dist/locales/translation_')
     .constant('LOCALES_SUFIX', '.json');
 
   angular.module('risevision.displaysApp.config', [])
